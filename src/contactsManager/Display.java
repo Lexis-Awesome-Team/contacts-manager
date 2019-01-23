4d8a5224321f2b516b76b361ca4d1caa83e65188
Lexi Morgan <leximorgan816@gmail.com>
2019-01-23 10:59:29 -0600

    added search feature by name and number

--- src/contactsManager/Display.java
+++ src/contactsManager/Display.java
@@ -24,7 +24,7 @@ class Display {
    static String getDisplay(){
        return"1. View Contacts.\n" +
        "2. Add a new contact.\n" +
-       "3. Search a contact by name.\n" +
+       "3. Search for a contact.\n" +
        "4. Delete an existing account.\n" +
        "5. Exit. \n\n"+
        "Enter an option (1, 2, 3, 4, 5)\n";
@@ -39,5 +39,43 @@ class Display {
        }
    }
 
+
+   static void searchContacts(){
+       int searchChoice = Manager.userInput.getInt("How would you like to search?\n 1. Name\n 2. Number\n");
+       switch(searchChoice){
+           case 1:
+           // search by name
+               System.out.println("Search by Name:");
+               boolean contactExists = false;
+               String nameSearch = Manager.userInput.getString("Please enter a name:");
+               for(String contact: contacts ){
+                   if (contact.substring(0, contact.indexOf("|")).contains(nameSearch)){
+                       contactExists = true;
+                       System.out.println(contact);
+                   }
+               }
+               if(!contactExists) {
+                   System.out.println("No contact matching that name;");
+               }
+               break;
+
+           case 2:
+           // search by number
+           System.out.println("Search by Number:");
+           contactExists = false;
+           nameSearch = Manager.userInput.getString("Please enter a number:");
+           for(String contact : contacts ){
+               if (contact.substring(contact.indexOf("|"), contact.length()-1).contains(nameSearch)){
+                   contactExists = true;
+                   System.out.println(contact);
+               }
+           }
+           if(!contactExists) {
+               System.out.println("No contact matching that number;");
+           }
+           break;
+       }
+
+   }
 
 }
--- src/contactsManager/Manager.java
+++ src/contactsManager/Manager.java
@@ -10,19 +10,26 @@ public class Manager {
             int menuChoice = userInput.getInt(Display.getDisplay(), 1, 5);
             switch (menuChoice){
                 case 1:
+                    // View all contacts
                     System.out.println();
                     Display.viewContacts();
                     System.out.println();
                     break;
                 case 2:
+                    // Add a new contact
                     Editor.addContact();
                     break;
                 case 3:
+                    // Search for a contact
+                    Display.searchContacts();
                     break;
                 case 4:
+                    // Delete a contact
                     break;
                 case 5:
+                    // Exit the app
                     appRunning = false;
+                    System.out.println("Goodbye.");
                     break;
             }
         }
